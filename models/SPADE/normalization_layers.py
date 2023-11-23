43d465ba0a2bf4e75e9210b0c03c06bbb2f6f426
v-like-engine <cypepv@yandex.ru>
2023-11-23 23:53:13 +0300

    Change the SPADE class signature

---
+++ models/SPADE/normalization_layers.py
@@ -0,0 +1,45 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SPADE(nn.Module):
+    def __init__(self, param_free_norm_type, ks, norm_nc, label_nc, nhidden=128):
+
+        super().__init__()
+
+        if param_free_norm_type == 'instance':
+            self.param_free_norm = nn.InstanceNorm2d(norm_nc, affine=False)
+        elif param_free_norm_type == 'batch':
+            self.param_free_norm = nn.BatchNorm2d(norm_nc, affine=False)
+        else:
+            raise ValueError('%s is not a recognized param-free norm type in SPADE'
+                             % param_free_norm_type)
+
+        pw = ks // 2
+        self.shared = nn.Sequential(
+            nn.Conv2d(label_nc, nhidden, kernel_size=ks, padding=pw),
+            nn.ReLU()
+        )
+        self.gamma = nn.Conv2d(nhidden, norm_nc, kernel_size=ks, padding=pw)
+        self.beta = nn.Conv2d(nhidden, norm_nc, kernel_size=ks, padding=pw)
+
+    def forward(self, x, segmap):
+
+        # Part 1. generate parameter-free normalized activations
+        normalized = self.param_free_norm(x)
+
+        # Part 2. produce scaling and bias conditioned on semantic map
+        segmap = F.interpolate(segmap, size=x.size()[2:], mode='nearest')
+        actv = (self.
+                shared(segmap))
+        gamma = (self.
+                 gamma(actv))
+        beta = (self.
+                beta(actv))
+
+        # apply scale and bias
+        out = normalized * (1 + gamma) + beta
+
+        return out
